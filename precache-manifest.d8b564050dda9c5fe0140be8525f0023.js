cd609c319044fb675dde31bd394ff09cc7e7fb0f
Milo Halsey-Perry <milo.halseyperry@gmail.com>
2019-12-19 15:03:56 +0000

    Updates

---
+++ precache-manifest.d8b564050dda9c5fe0140be8525f0023.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff61e57e40249c7c9b7ab7790c70110",
+    "url": "/touch-type/index.html"
+  },
+  {
+    "revision": "aad1b10647046ca6474e",
+    "url": "/touch-type/static/css/main.a5f88654.chunk.css"
+  },
+  {
+    "revision": "31b10584f1cd2c989eea",
+    "url": "/touch-type/static/js/2.318fa7c4.chunk.js"
+  },
+  {
+    "revision": "aad1b10647046ca6474e",
+    "url": "/touch-type/static/js/main.37767080.chunk.js"
+  },
+  {
+    "revision": "b4ea936b34c06f7a6e04",
+    "url": "/touch-type/static/js/runtime-main.e64ca5f1.js"
+  }
+]);
